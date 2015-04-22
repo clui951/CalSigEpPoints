d55b11c5f7bb7d22a78da27e9aedff97858e451a
Calvin Lui <clui951@berkeley.edu>
2015-04-22 02:44:28 -0700

    signin/out notification generated; need place to display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
+    flash[:notif] = "Signed In!"
     redirect_to root_path, :notice => "Signed In!"
   end
 
   def destroy
     session[:user_id] = nil
+	flash[:notif] = "Signed Out!"
     redirect_to root_path, :notice => "Signed Out!"
   end
 
