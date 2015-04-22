0503094c1b64fd5026655e9768b67ca00e4ff889
Calvin Lui <clui951@berkeley.edu>
2015-04-21 23:55:07 -0700

    Google Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  root to:'home#index'
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+
+  # get 'auth/:provider', to: 'sessions#create'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: "home#index"
   get 'users', to:'users#users'
   get 'tasks/new', to:'tasks#new'
+
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421220742) do
+ActiveRecord::Schema.define(version: 20150422060629) do
 
   create_table "tasks", force: true do |t|
     t.text     "title"
@@ -23,9 +23,11 @@ ActiveRecord::Schema.define(version: 20150421220742) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
     t.string   "name"
-    t.string   "email"
-    t.string   "year"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
