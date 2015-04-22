752adff0011019708bdbc959b45fbc7d6d6bb199
Claire <lee.claire@berkeley.edu>
2015-04-21 23:33:53 -0700

    made rushnames

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root to:'home#index'
   get 'users', to:'users#users'
   get 'tasks/new', to:'tasks#new'
+  get 'rushnames', to: 'rushnames#index'
+  get 'rushnames/new', to: 'rushnames#new'
+  post 'rushnames', to: 'rushnames#create'
 end
 
 
-# rails g model Task title:text comment:text value:integer 
+# rails g model Task title:text comment:text value:integer
