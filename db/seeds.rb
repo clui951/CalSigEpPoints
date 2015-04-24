cc04dd374e1557e727a7100a3bfd6768bde02bbd
Claire <lee.claire@berkeley.edu>
2015-04-23 22:42:51 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Task.create(title: 'Gym', comment: 'RSF with Stevie', value: 5)
+Task.create(title: 'Wash dishes', comment: 'Wednesday dinner', value: 10)
+Task.create(title: 'Clean bathroom', comment: 'gross', value: 50)
+
+
+%w(Soap Ketchup Mustard Beer).each do |item|
+  Reimbursement.create item: item, date: '4/25/2015', cost: rand(1..20), VP: 'Stevie'
+end
+
+%w(Andy Ben Connor Dylan).each do |name|
+  Rushname.create name: name, photo: name+".jpeg", email: name+"@yahoo.com", phone: "(555) 555-5555", description: "Cool dude!"
+end
