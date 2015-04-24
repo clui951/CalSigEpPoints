84218114c88545978b28740591e974598cc56da7
Calvin Lui <clui951@berkeley.edu>
2015-04-23 20:45:33 -0700

    omniauth successful key

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,8 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
     flash[:notif] = "Signed In!"
-    if not current_user.organization
-      redirect_to 'organization/new'
-    else 
-      redirect_to root_path, :notice => "Signed In!"
-    end
+    redirect_to root_path, :notice => "Signed In!"
+    # end
   end
 
   def destroy
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger 
 
 Rails.application.config.middleware.use OmniAuth::Builder do 
-	provider :google_oauth2, '886997338194-4e39g1heo3nb61drsltve3chafbclcel.apps.googleusercontent.com', 'xp2MuuzydEpCx65EyAKRrzgl', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}} 
+	provider :google_oauth2, '886997338194-pevc1mgffb48t259pm50s8ciue6f1810.apps.googleusercontent.com', 'DzvP_oVg0b0ObaaUbI0ZnrfA', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}} 
 end
