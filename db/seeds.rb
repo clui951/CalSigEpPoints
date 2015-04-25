0325944805ad7079554801b3c77a5c368a3d7c94
Stevie Kobori <skobori@gmail.com>
2015-04-24 21:11:35 -0700

    fixed seed syntax errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-organizations = Organization.create(name: 'The Rails Train')
+organization = Organization.create(name: 'The Rails Train')
 
-users = User.create([{ name: 'Calvin', organization: organizations.first }, 
-					{ name: 'Claire', organization: organizations.first }, 
-					{ name: 'Mei', organization: organizations.first }, 
-					{ name: 'Stevie', organization: organizations.first }])
+users = User.create([{ name: 'Calvin', organization: organization.id }, 
+					{ name: 'Claire', organization: organization.id }, 
+					{ name: 'Mei', organization: organization.id }, 
+					{ name: 'Stevie', organization: organization.id }])
 
-tasks = Task.create([{title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[0]}, 
-			{title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[1]},
-			{title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[2]},
-			{title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[3]},
-			{title: 'Tabled for the Rails Train', comment: 'Chugga Chugga', value: 15, user_id: users[0]}
-			{title: 'Bought train food', comment: 'Nom Nom Nom', value: 10, user_id: users[0]}])
+tasks = Task.create([{ title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[0] }, 
+			{ title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[1] },
+			{ title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[2] },
+			{ title: 'Rode the Rails Train', comment: 'Choo Choo', value: 10, user_id: users[3] },
+			{ title: 'Tabled for the Rails Train', comment: 'Chugga Chugga', value: 15, user_id: users[0] },
+			{ title: 'Bought train food', comment: 'Nom Nom Nom', value: 10, user_id: users[0] }])
 
 %w(Soap Ketchup Mustard Beer).each do |item|
   Reimbursement.create item: item, date: '4/25/2015', cost: rand(1..20), VP: 'Stevie'
